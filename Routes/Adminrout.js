77d057490d485765d3f90e112e033cd9fee84c6e
hxriif <zaidbnuarifc@gmail.com>
2023-12-14 10:45:40 +0530

    update_

--- Routes/Adminrout.js
+++ Routes/Adminrout.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const admincontroller = require("../controllers/Admincontroller");
 const TrycatchMiddleware = require("../middlewares/Trycatchmiddleware");
 const verifyToken = require("../middlewares/adminAuthMiddleware");
+const imageuploading=require("../middlewares/image_uploader/img_uploader")
 
 router
   .post("/login", TrycatchMiddleware(admincontroller.login))
@@ -11,7 +12,7 @@ router
 
   .get("/users", TrycatchMiddleware(admincontroller.allusers))
   .get("/users/:id", TrycatchMiddleware(admincontroller.getUserById))
-  .post("/add", TrycatchMiddleware(admincontroller.addProduct))
+  .post("/add",imageuploading, TrycatchMiddleware(admincontroller.addProduct))
   .delete("/delete/:id", TrycatchMiddleware(admincontroller.deleteproduct))
   .get("/view", TrycatchMiddleware(admincontroller.allproducts))
   .put("/update", TrycatchMiddleware(admincontroller.adminUpdate));
